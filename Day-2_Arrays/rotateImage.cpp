2848e06daa04418b469b4ac6e08fca3e1cf36702
Samshul Ajam Ansari <sajam8948394283@gmail.com>
2023-11-02 14:14:28 +0530

    Ritate Image

---
+++ Day-2_Arrays/rotateImage.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/rotate-image/description/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // find the transpose
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // reverse each row
+        for(int i=0; i<n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
